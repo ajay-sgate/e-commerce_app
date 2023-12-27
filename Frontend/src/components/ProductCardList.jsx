18cc937061a7a9936883cf72a7f8a72af2ca4f52
ajay <ajay@sgate.net>
2023-12-27 17:57:02 +0530

    pagination added

--- Frontend/src/components/ProductCardList.jsx
+++ Frontend/src/components/ProductCardList.jsx
@@ -1,14 +1,16 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import ProductCard from './ProductCard'
-import { Box, Grid, Skeleton } from '@mui/material'
+import { Box, Grid, Pagination, Skeleton } from '@mui/material'
 
 const ProductCardList = () => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(false)
+    const [skip, setSkip] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
     const fetchData = () => {
         setLoading(true)
-        axios.get(`https://dummyjson.com/products?limit=100`)
+        axios.get(`https://dummyjson.com/products?skip=${skip}&limit=20`)
             .then((res) => {
                 setProducts(res.data.products)
                 setLoading(false)
@@ -19,9 +21,14 @@ const ProductCardList = () => {
             })
     }
 
+    const handleChange = (event, value) => {
+        setSkip((value - 1) * 20)
+        setCurrentPage(value)
+    }
+
     useEffect(() => {
         fetchData()
-    }, [])
+    }, [currentPage])
 
 
     return (
@@ -39,7 +46,7 @@ const ProductCardList = () => {
                     rowSpacing={3}
                     columnSpacing={{ xs: 1, sm: 2, md: 3 }}
                 >
-                    {Array.from({ length: 100 }).map((_, index) => (
+                    {Array.from({ length: 20 }).map((_, index) => (
                         <Grid key={index} item md={3}> {/* Removed width={"20%"} */}
                             <Skeleton variant="rectangular" width={210} height={118} />
                             <Box padding={"10px"}>
@@ -55,12 +62,12 @@ const ProductCardList = () => {
                             </Box>
                         </Grid>
                     ))}
-                </Grid> : <Grid
+                </Grid> : <><Grid
                     container
                     alignItems="center"
                     justifyContent="center"
                     px={"20px"}
-                    py={"150px"}
+                    paddingTop={"150px"}
                     rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}
                 >
                     {products.map((data) => (
@@ -68,7 +75,15 @@ const ProductCardList = () => {
                             <ProductCard data={data} />
                         </Grid>
                     ))}
-                </Grid>}
+                </Grid>
+                    <Box display="flex" justifyContent="center" my={3} >
+                        <Pagination count={5} color="primary" size='large'
+                            page={currentPage}
+                            sx={{ '& .MuiPaginationItem-root': { fontSize: '1.5rem' } }}
+                            onChange={handleChange}
+                        />
+                    </Box>
+                </>}
         </>
     )
 }
